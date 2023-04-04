2a278d9956703b71945766d5752e89f79c9f790b
jagernau <stepanenkov25@gmail.com>
2023-04-04 22:55:30 +0200

    рабочая версия, но надо рефакторить

--- copy_pixel.py
+++ copy_pixel.py
@@ -1,7 +1,8 @@
-
+import pandas as pd
 import cv2 as cv
 import math
 import numpy as np
+import sys
 
 
 class Griding:
@@ -12,7 +13,7 @@ class Griding:
         self.griding_width = int()
         self.picture_numbers = self.griding_height * self.griding_width
         self.converted = self.__convert_img()
-        self.splited_pic = self.__split_pic_in_little_pics()
+        self.splited_pic = self.split_pic_in_little_pics()
 
     
     def __convert_img(self):
@@ -20,31 +21,29 @@ class Griding:
         self.griding_height = math.floor(converted.shape[0] / self.grid)
         self.griding_width = math.floor(converted.shape[1] / self.grid)
         return converted
-
-
-    def __split_pic_in_little_pics(self):
-        new_len = []
+    
+    def rgb(self, long_list):
+        blue = 0
+        green = 0
+        red = 0
+        for i in long_list:
+            for b in i:
+                blue += b[2]
+                green += b[1]
+                red += b[0]
+        return [round(red / (self.grid*self.grid)), round(green / (self.grid*self.grid)), round(blue / (self.grid*self.grid))]
+ 
+            
+    def split_pic_in_little_pics(self):
+        new_len = np.full((self.converted.shape[0], self.converted.shape[1], 3), [0,0,0], dtype=np.uint8)
         for x in range(0,self.converted.shape[0], self.grid):
             for y in range(0,self.converted.shape[1], self.grid):                   
-                new_len.append(self.converted[x:x + self.grid, y:y + self.grid])
+                new_len[x:x + self.grid, y:y + self.grid] = self.rgb(self.converted[x:x + self.grid, y:y + self.grid])
         return new_len
 
 
-class CreatePic:
-    def __init__(self, cls: Griding):
-        self.cls = cls
-        self.created_pic_array = self.__generate_pic_array()
-
-    def __generate_pic_array(self):
-        return np.full((self.cls.converted.shape[0], self.cls.converted.shape[1], 3), [0,0,0], dtype=np.uint8)
-    def get_created_pic_array(self):
-        return self.created_pic_array
 
 
-try:
-    pix_art = Griding('1999 Aphrodite (front).jpg', 10)
-except BaseException:
-    print("Нет такого файла")
-else:
-    creat = CreatePic(pix_art)
-    cv.imwrite("new2.png", creat.get_created_pic_array())
+pix_art = Griding('1999 Aphrodite (front).jpg', 20)
+cv.imwrite("new4.jpg", pix_art.split_pic_in_little_pics())
+#    print(len(pix_art.split_pic_in_little_pics()))
