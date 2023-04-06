3ef5e748dcf34690a65dd28536189278724792f4
jagernau <stepanenkov25@gmail.com>
2023-04-06 23:03:33 +0200

    авто анализ файла

--- copy_pixel.py
+++ copy_pixel.py
@@ -1,32 +1,39 @@
-import pandas as pd
 import cv2 as cv
 import math
 import numpy as np
-import sys
+import os
+import glob
 
+class GridingPic:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    IN_PICS_DIR = "pics/" # Input directory containing pictures
+    OUT_PICS_DIR = "out_pics/" # Output directory for storing processed pictures
 
-class Griding:
-    def __init__(self, name: str, grid: int) -> None:
-        self.name = name
+    def __init__(self, grid: int, file_name=None) -> None:
+        self.file_name = file_name
         self.grid = grid
         self.griding_height = int()
         self.griding_width = int()
         self.picture_numbers = self.griding_height * self.griding_width
         self.converted = self.__convert_img()
-        self.splited_pic = self.split_pic_in_little_pics()
 
     
-    def __convert_img(self):
-        converted = cv.imread(f"pics/{self.name}")
+    def __convert_img(self) -> np.ndarray:
+        if self.file_name != None:
+            filepath = os.path.join(self.BASE_DIR, self.IN_PICS_DIR + self.file_name)
+        if self.file_name == None:
+            filepath = os.path.join(self.BASE_DIR,self.IN_PICS_DIR, max(glob.iglob(os.path.join(self.BASE_DIR, self.IN_PICS_DIR, "*")), key=os.path.getctime))
+        converted = cv.imread(filepath)
         self.griding_height = math.floor(converted.shape[0] / self.grid)
         self.griding_width = math.floor(converted.shape[1] / self.grid)
         return converted
-    
-    def rgb(self, long_list):
+   
+
+    def __sum_rgb(self, one_square: np.ndarray) -> list[int]:
         blue = 0
         green = 0
         red = 0
-        for i in long_list:
+        for i in one_square:
             for b in i:
                 blue += b[2]
                 green += b[1]
@@ -34,16 +41,12 @@ class Griding:
         return [round(red / (self.grid*self.grid)), round(green / (self.grid*self.grid)), round(blue / (self.grid*self.grid))]
  
             
-    def split_pic_in_little_pics(self):
-        new_len = np.full((self.converted.shape[0], self.converted.shape[1], 3), [0,0,0], dtype=np.uint8)
+    def grid_pic_in_little_pics(self) -> np.ndarray:
+        ready_grid_array = np.full((self.converted.shape[0], self.converted.shape[1], 3), [0,0,0], dtype=np.uint8)
         for x in range(0,self.converted.shape[0], self.grid):
             for y in range(0,self.converted.shape[1], self.grid):                   
-                new_len[x:x + self.grid, y:y + self.grid] = self.rgb(self.converted[x:x + self.grid, y:y + self.grid])
-        return new_len
+                ready_grid_array[x:x + self.grid, y:y + self.grid] = self.__sum_rgb(self.converted[x:x + self.grid, y:y + self.grid])
+        return ready_grid_array
 
-
-
-
-pix_art = Griding('1999 Aphrodite (front).jpg', 20)
-cv.imwrite("new4.jpg", pix_art.split_pic_in_little_pics())
-#    print(len(pix_art.split_pic_in_little_pics()))
+pix_art = GridingPic(50)
+cv.imwrite("new4.jpg", pix_art.grid_pic_in_little_pics())
